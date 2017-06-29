8af147e7d105317769ebffa1e88f1ee876982bc7
Vu Anh Hao <vuanhhaogk@gmail.com>
2017-06-29 19:58:24 +0700

    Add level 2-8

--- app/assets/js/game.js
+++ app/assets/js/game.js
@@ -68,52 +68,80 @@ var gameState = {
         this.gameover_subtext = game.add.text(0, Math.floor(game.world.height / 2) + 10, 'Press spacebar to replay!', { font: '16px monospace', fill: '#ffffff' });
         this.gameover_subtext.x = Math.floor((game.world.width - this.gameover_subtext.width) / 2);
         this.gameover_subtext.visible = false;
+        this.win_text = game.add.text(0, Math.floor((game.world.height - 32) / 2), 'You win!', { font: '32px monospace', fill: '#ffffff' });
+        this.win_text.x = Math.floor((game.world.width - this.win_text.width) / 2);
+        this.win_text.visible = false;
+        this.pause_text = game.add.text(0, Math.floor((game.world.height - 32) / 2), 'Paused!', { font: '32px monospace', fill: '#ffffff' });
+        this.pause_text.x = Math.floor((game.world.width - this.pause_text.width) / 2);
+        this.pause_text.visible = false;
         this.target_text.text = 'Target: ' + this.target;
+        this.pause = false;
     },
     update: function () {
         var hit_platform = game.physics.arcade.collide(this.player, this.walls);
-        game.physics.arcade.overlap(this.player, this.traps, this.gameover, null, this);
+        game.physics.arcade.collide(this.player, this.traps, this.gameover, null, this);
         game.physics.arcade.overlap(this.player, this.lavas, this.gameover, null, this);
         game.physics.arcade.overlap(this.player, this.coins, this.collectCoin, null, this);
-        if (this.player.alive) {
-            if (this.cursors.left.isDown) {
-                this.player.body.velocity.x = -150;
-            }
-            else if (this.cursors.right.isDown) {
-                this.player.body.velocity.x = 150;
-            }
-            else {
-                this.player.body.velocity.x = 0;
-            }
-            if (this.cursors.up.isDown && hit_platform && this.player.body.touching.down) {
-                this.player.body.velocity.y = -350;
-            }
+        if (!this.player.alive || this.pause)
+            return;
+        if (this.cursors.left.isDown) {
+            this.player.body.velocity.x = -150;
+        }
+        else if (this.cursors.right.isDown) {
+            this.player.body.velocity.x = 150;
+        }
+        else {
+            this.player.body.velocity.x = 0;
+        }
+        if (this.cursors.up.isDown && hit_platform && this.player.body.touching.down) {
+            this.player.body.velocity.y = -350;
         }
     },
     gameover: function () {
-        this.gameover_text.visible = true;
-        this.gameover_subtext.visible = true;
-        this.player.alive = false;
-    },
-    pressSpace: function () {
-        if (this.player.alive === false) {
-            game.global.level = 0;
-            game.state.start('game');
+        if (this.target !== 0) {
+            this.gameover_text.visible = true;
+            this.gameover_subtext.visible = true;
+            this.player.alive = false;
         }
     },
+    pressSpace: function () {
+        if (this.player.alive === false || this.target === 0) {
+            game.global.level = 0;
+            game.state.start('game');
+            return;
+        }
+        this.pauseGame();
+    },
+    pauseGame: function () {
+        var _this = this;
+        this.pause = !this.pause;
+        game.physics.arcade.isPaused = this.pause;
+        this.pause_text.visible = this.pause;
+        setTimeout(function () { return game.lockRender = _this.pause; }, 100);
+    },
     collectCoin: function (player, coin) {
+        var _this = this;
+        if (!this.player.alive)
+            return;
         var coin_tween = game.add.tween(coin.scale);
         coin_tween.to({ x: 0, y: 0 }, 100, Phaser.Easing.Linear.None);
-        coin_tween.onComplete.addOnce(function () { return coin.kill(); });
+        coin_tween.onComplete.addOnce(function () {
+            coin.kill();
+            if (_this.target === 0) {
+                if (game.cache.getJSON('levels')[game.global.level + 1]) {
+                    game.global.level++;
+                    game.state.start('game');
+                }
+                else {
+                    _this.win_text.visible = true;
+                }
+            }
+        });
         coin_tween.start();
         if (coin.alive) {
             this.target--;
             this.target_text.text = 'Target: ' + this.target;
             coin.alive = false;
-        }
-        if (this.target === 0 && game.cache.getJSON('levels')[game.global.level + 1]) {
-            game.global.level++;
-            game.state.start('game');
         }
     }
 };
