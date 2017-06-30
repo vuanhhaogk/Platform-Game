e31168d8738265ce1b13f8282f142484760a4402
Vu Anh Hao <vuanhhaogk@gmail.com>
2017-06-30 19:20:21 +0700

    Add info screen, new level 1-7, key

--- src/ts/game.ts
+++ src/ts/game.ts
@@ -2,23 +2,66 @@ let gameState = {
     preload: function(){
 
     },
-    create: function(){
-        game.add.sprite(0, 0, 'background')
+    createInfo: function(){
+        let basic_style = { font: '12px monospace', fill: '#ffffff' }
+        let large_style = { font: '32px monospace', fill: '#ffffff' }
 
+        let target = 0
+
+        let level_text = game.add.text(10, 10, `Level: ${game.global.level + 1}/${game.cache.getJSON('levels').length}`, basic_style)
+        level_text.fixedToCamera = true
+
+        let target_text = game.add.text(level_text.width + 20, 10, 'Target: 0', basic_style)
+        target_text.fixedToCamera = true
+
+        let message_text = game.add.text(Math.floor(game.camera.width/2), Math.floor((game.camera.height - 32)/2), '', large_style)
+        message_text.anchor.x = .5
+        message_text.fixedToCamera = true
+
+        let tip_text = game.add.text(0, game.camera.height - 22, 'LEFT|RIGHT ARROW: Move   &   UP ARROW: Jump   &   SPACEBAR: play/pause   &   ESC: Menu   &   R: Replay', basic_style)
+        tip_text.x = Math.floor((game.camera.width - tip_text.width)/2)
+        tip_text.fixedToCamera = true
+
+        let info = {}
+
+        Object.defineProperty(info, 'target', {
+            get(){
+                return target
+            },
+            set(ntarget){
+                target = ntarget
+                target_text.text = 'Target: ' + target
+            }
+        })
+
+        Object.defineProperty(info, 'message', {
+            get(){
+                return message_text.text
+            },
+            set(nmessage){
+                message_text.text = nmessage
+            }
+        })
+
+        this.info = info
+    },
+    create: function(){
+        // world
+        game.world.setBounds(0, 0, 2000, 2000)
+        game.add.sprite(0, 0, 'background').fixedToCamera = true
+
+        // map
         let map: string[] = game.cache.getJSON('levels')[game.global.level]
         let width: number = map[0].length
         let height: number = map.length
 
-        let margin_left: number = (game.world.width - game.const.BSIZE * width)/2
-        let margin_top: number = (game.world.height - game.const.BSIZE * height)/2
+        let margin_left: number = (game.world.width - 20 * width)/2
+        let margin_top: number = (game.world.height - 20 * height)/2
 
-        let level_text = game.add.text(10, 10, `Level: ${game.global.level + 1}/${game.cache.getJSON('levels').length}`, { font: '12px monospace', fill: '#ffffff' })
-
-        this.target = 0
-        this.target_text = game.add.text(level_text.width + 30, 10, 'Target: 0', { font: '12px monospace', fill: '#ffffff' })
-
+        // physic
         game.physics.startSystem(Phaser.Physics.ARCADE)
 
+        // object
         this.traps = game.add.group()
         this.traps.enableBody = true
 
@@ -26,6 +69,7 @@ let gameState = {
         game.physics.arcade.enable(this.player)
         this.player.body.gravity.y = 1000
         this.player.alive = true
+        game.camera.follow(this.player)
 
         this.walls = game.add.group()
         this.walls.enableBody = true
@@ -36,10 +80,12 @@ let gameState = {
         this.coins = game.add.group()
         this.coins.enableBody = true
 
+        this.createInfo()
+
         for (let r: number = 0; r < height; r++)
             for (let c: number = 0; c < width; c++){
-                let x: number = c * game.const.BSIZE + margin_left
-                let y: number = r * game.const.BSIZE + margin_top
+                let x: number = c * 20 + margin_left
+                let y: number = r * 20 + margin_top
 
                 switch (map[r][c]){
                     case 'X':
@@ -60,12 +106,12 @@ let gameState = {
                         let coin = this.coins.create(x, y, 'coin')
                         coin.alive = true
                         coin.anchor.setTo(.5)
-                        coin.x += game.const.BSIZE/2
-                        coin.y += game.const.BSIZE/2
+                        coin.x += 20/2
+                        coin.y += 20/2
                         coin.animations.add('blink', [0, 1, 2, 3, 2, 1], 12, true)
                         coin.animations.play('blink')
                         coin.body.immovable = true
-                        this.target++
+                        this.info.target++
                         break
                     case 'T':
                         let trap = this.traps.create(x, y, 'trap')
@@ -74,27 +120,18 @@ let gameState = {
                 }
             }
 
+        // keyboard
         this.cursors = game.input.keyboard.createCursorKeys()
         game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR).onDown.add(this.pressSpace, this)
+        game.input.keyboard.addKey(Phaser.Keyboard.R).onDown.add(() => game.state.start('game'), this)
 
-        this.gameover_text = game.add.text(0, Math.floor(game.world.height/2) - 20, 'Gameover!', { font: '32px monospace', fill: '#ffffff' })
-        this.gameover_text.x = Math.floor((game.world.width - this.gameover_text.width)/2)
-        this.gameover_text.visible = false
-        this.gameover_subtext = game.add.text(0, Math.floor(game.world.height/2) + 10, 'Press spacebar to replay!', { font: '16px monospace', fill: '#ffffff' })
-        this.gameover_subtext.x = Math.floor((game.world.width - this.gameover_subtext.width)/2)
-        this.gameover_subtext.visible = false
-
-        this.win_text = game.add.text(0, Math.floor((game.world.height - 32)/2), 'You win!', { font: '32px monospace', fill: '#ffffff' })
-        this.win_text.x = Math.floor((game.world.width - this.win_text.width)/2)
-        this.win_text.visible = false
-
-        this.pause_text = game.add.text(0, Math.floor((game.world.height - 32)/2), 'Paused!', { font: '32px monospace', fill: '#ffffff' })
-        this.pause_text.x = Math.floor((game.world.width - this.pause_text.width)/2)
-        this.pause_text.visible = false
-
-        this.target_text.text = 'Target: ' + this.target
-
-        this.pause = false
+        // mode
+        this.mode = 'preplay'
+        this.info.message = `Level: ${game.global.level + 1}`
+        setTimeout(() => {
+            this.info.message = ''
+            this.mode = 'play'
+        }, 1000)
     },
     update: function(){
         let hit_platform = game.physics.arcade.collide(this.player, this.walls)
@@ -103,7 +140,7 @@ let gameState = {
         game.physics.arcade.overlap(this.player, this.lavas, this.gameover, null, this)
         game.physics.arcade.overlap(this.player, this.coins, this.collectCoin, null, this)
 
-        if (!this.player.alive || this.pause)
+        if (this.mode !== 'play' && this.mode !== 'preplay')
             return
 
         if (this.cursors.left.isDown){
@@ -119,20 +156,25 @@ let gameState = {
         }
     },
     gameover: function(){
-        if (this.target !== 0){
-            this.gameover_text.visible = true
-            this.gameover_subtext.visible = true
-            this.player.alive = false
+        if (this.mode !== 'gameover' && this.mode !== 'finish'){
+            this.mode = 'gameover'
+            this.info.message = 'Gameover'
         }
     },
     pressSpace: function(){
-        if (this.player.alive === false || this.target === 0){
-            game.global.level = 0
-            game.state.start('game')
+        if (this.mode !== 'pause' && this.mode !== 'play')
+            return
+            
+        if (this.mode === 'pause'){
+            this.mode = 'play'
+            this.info.message = ''
+            game.physics.arcade.isPaused = false
             return
         }
 
-        this.pauseGame()
+        this.mode = 'pause'
+        this.info.message = 'Pause'
+        game.physics.arcade.isPaused = true
     },
     pauseGame: function(){
         this.pause = !this.pause
@@ -150,20 +192,21 @@ let gameState = {
         coin_tween.onComplete.addOnce(() => {
             coin.kill()
 
-            if (this.target === 0){
-                this.target = -1
-                if (game.cache.getJSON('levels')[game.global.level + 1]){
-                    game.global.level ++
-                    game.state.start('game')
+            if (this.info.target === 0 && this.mode !== 'finish'){
+                this.mode = 'finish'
+                game.global.level++
+                if (game.cache.getJSON('levels')[game.global.level]){
+                    this.info.message = 'Complete'
+                    setTimeout(() => game.state.start('game'), 1000)
                 } else {
-                    this.win_text.visible = true
+                    this.info.message = 'Win'
                 }
             }
         })
+
         coin_tween.start()
         if (coin.alive){
-            this.target --
-            this.target_text.text = 'Target: ' + this.target
+            this.info.target --
             coin.alive = false
         }
     }
